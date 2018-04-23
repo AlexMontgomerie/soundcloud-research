573dc9b11b5dddbff9be9123d888e80dcfadb621
Alex Montgomerie <AlexMontgomerie@github.com>
2018-04-23 18:52:36 +0100

    adding support for retrieving individual user information

--- parseUserHTML.py
+++ parseUserHTML.py
@@ -1,13 +1,12 @@
 #import libraries: json, re, url
 import re
 import json
-import urllib
-
+import urllib.request
 #list of search result items
 
 
 #open up html files
-filename = "./soundcloud-hiphop.html"
+filename = "./soundcloud-london.html"
 f = open(filename,"r")
 '''
 for line in f:
@@ -17,7 +16,7 @@ for line in f:
 '''
 
 userList = []
-
+count=0
 #load first line
 line = f.readline()
 while line:
@@ -34,16 +33,32 @@ while line:
     match_tmp = re.match("(\ \ \ \ <a href=\"\/)([a-z,A-Z,0-9,\-]*)",line) #TODO: find 
     userTemplate['username'] = match_tmp.group(2) 
     print(userTemplate['username'])
+    
+    '''
     #the rest are optional
-
+    count += 1
     pro       = re.match("",line) #TODO: match for pro account
     location  = re.match("",line) #TODO: match for location
-    followers = re.match("(<li title=\")([0-9,\.]+)( followers)",line) #TODO: match for number of followers
-    tracks    = re.match("(<li title=\")([0-9,\.]+)(\ tracks)",line) #TODO: match for number of tracks
+    followers_match = re.match("(<li title=\")([0-9,\.]+)( followers)",line) #TODO: match for number of followers
+    tracks_match    = re.match("(<li title=\")([0-9,\.]+)(\ tracks)",line) #TODO: match for number of tracks
 
-    while not (pro or location or followers or tracks):
+    if followers_match:    
+      followers = followers_match.group(2)
+    if tracks_match:
+      tracks    = tracks_match.group(2)
+    '''
+    print("http://soundcloud.com/" + userTemplate['username'])
+    req = urllib.request.Request('https://soundcloud.com/robotrump') 
+    response = urllib.request.urlopen('https://soundcloud.com/robotrump')
+    html = response.read()
+    profile_json_match = re.match("(  <script>webpackJsonp\(\[\],)(.*?)(\);<\/script>)",html)
+    print(profile_json_match)
+
+
+'''
+    while not (followers_match or tracks_match):
       #if new section, break
-      if re.match("<div class=\"sc-media-content\">"):
+      if re.match("<div class=\"sc-media-content\">", line):
         #update userList
         break
       #location (3 lines after   <h3 class="userItem__details sc-type-light">)
@@ -53,21 +68,21 @@ while line:
       
       pro       = re.match("",line) #TODO: match for pro account
       location  = re.match("",line) #TODO: match for location
-      followers = re.match("",line) #TODO: match for number of followers
-      tracks    = re.match("",line) #TODO: match for number of tracks
+      followers_match = re.match("(<li title=\")([0-9,\.]+)( followers)",line) #TODO: match for number of followers
+      tracks_match    = re.match("(<li title=\")([0-9,\.]+)(\ tracks)",line) #TODO: match for number of tracks
 
-      if pro:
-        userTemplate["pro"] = pro
+      if followers_match:    
+        followers = followers_match.group(1)
+        print(followers)
+      if tracks_match:
+        tracks    = tracks_match.group(1)
+        print(tracks)      
 
-      if location:
-        userTemplate["location"] = location
+     #update userList
+'''
 
-      if followers:
-        userTemplate["followers"] = followers
-      
-      if tracks:
-        userTemplate["tracks"] = tracks
-
-    #update userList
+# RegEx for getting user json field
+# (  <script>webpackJsonp\(\[\],)(.*?)(\);<\/script>)
 
 print("DONE!")
+print(count)
