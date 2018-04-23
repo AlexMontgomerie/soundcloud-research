629711f2c6e87310663ef290d42a3e800d6d0f8f
Alex Montgomerie <AlexMontgomerie@github.com>
2018-04-23 12:58:30 +0100

    can parse username and follower/ track fields

--- parseUserHTML.py
+++ parseUserHTML.py
@@ -22,7 +22,7 @@ userList = []
 line = f.readline()
 while line:
   #reset user template
-  userTemplate = {"username" : "", "pro" : "", "location" : "", "followers" : "", "tracks" : ""}
+  userTemplate = {'username' : '', 'pro' : False, 'location' : '', 'followers' : 0, 'tracks' : 0}
   #print(line)
   #parse to find account username
   line = f.readline()
@@ -30,15 +30,26 @@ while line:
     #new user matched
     line = f.readline()
     #find username (after previous regex, form of href="/[str]" )
-    userTemplate[""] = re.match("",line) #TODO: find 
+    
+    match_tmp = re.match("(\ \ \ \ <a href=\"\/)([a-z,A-Z,0-9,\-]*)",line) #TODO: find 
+    userTemplate['username'] = match_tmp.group(2) 
+    print(userTemplate['username'])
     #the rest are optional
 
-    while !(pro || location || followers || tracks):
+    pro       = re.match("",line) #TODO: match for pro account
+    location  = re.match("",line) #TODO: match for location
+    followers = re.match("(<li title=\")([0-9,\.]+)( followers)",line) #TODO: match for number of followers
+    tracks    = re.match("(<li title=\")([0-9,\.]+)(\ tracks)",line) #TODO: match for number of tracks
+
+    while not (pro or location or followers or tracks):
       #if new section, break
       if re.match("<div class=\"sc-media-content\">"):
         #update userList
         break
       #location (3 lines after   <h3 class="userItem__details sc-type-light">)
+      
+      #read a new line
+      line = f.readline()
       
       pro       = re.match("",line) #TODO: match for pro account
       location  = re.match("",line) #TODO: match for location
